f862a49c07f40e1c1e48f988d5f8e79ce204452e
Alessandro Resta <alessandro.resta@proton.me>
2023-08-01 09:46:34 +0300

    Replace tests calling real api/repo by unit tests using mocks

--- chatbot_test.go
+++ chatbot_test.go
@@ -3,75 +3,108 @@ package chatbot
 import (
 	"context"
 	"io"
-	"net/http"
-	"os"
 	"strings"
 	"testing"
 
 	"github.com/alesr/chatbot/client/openaicli"
 	"github.com/alesr/chatbot/storage"
-	"github.com/jmoiron/sqlx"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestTrain(t *testing.T) {
-	client := openaicli.New(os.Getenv("OPENAI_API_KEY"), &http.Client{})
-
-	db, err := sqlx.Open(
-		"postgres",
-		"postgres://user:password@localhost:5432/testdb?sslmode=disable",
-	)
-	require.NoError(t, err)
-
-	repo := storage.NewPostgres(db)
-
-	svc := NewService(os.Getenv("OPENAI_API_KEY"), client, repo)
-
-	input := TrainInput{
-		UserID: "user1",
-		Model:  defaultModel,
-		Data: []io.Reader{
-			strings.NewReader(
-				`Unbearable Lightness of Being is an exploration of human life in its intricacies and contradictions. A poignant excerpt reads, "Human life occurs only once, and the reason we cannot determine which of our decisions are good and which are bad is that in a given situation we can make only one decision; we are not granted a second, third, or fourth life for comparison." It beautifully captures the essence of the book's exploration of existence, love, and choices.`,
-			),
-			strings.NewReader(
-				`Milan Kundera is a renowned Czech-born French author, known for his profound and philosophical narratives. His works often blend elements of history, politics, and existentialism. Kundera's most famous novel, "The Unbearable Lightness of Being," is celebrated for its introspective exploration of love and fate, reflecting his unique style of merging philosophy with fiction. He is esteemed for his insightful contributions to 20th-century literature.`,
-			),
-			strings.NewReader(
-				`Neptune, the eighth and farthest known planet from the Sun in our solar system, is known for its striking blue color, attributed to its atmosphere composed of hydrogen, helium, and methane. This gas giant, discovered in 1846, has a strong magnetic field, extreme weather conditions with the fastest wind speeds in the solar system, and a total of 14 known moons. The largest, Triton, is the only large moon in the solar system that orbits in the opposite direction of its planet's rotation, a retrograde orbit.`,
-			),
-			strings.NewReader(
-				`In Roman mythology, Neptune was the god of freshwater and the sea, a counterpart to the Greek god Poseidon. Known for his powerful and volatile nature, he ruled all bodies of water and was often depicted holding a trident. Neptune was also one of the chief gods and was considered the brother of Jupiter and Pluto. The Romans held a festival, Neptunalia, in his honor, which was marked by games and water-based festivities. It's fascinating to note the parallels between the turbulent and mysterious nature of the planet and its namesake god.`,
-			),
+	client := mockClient{
+		CreateEmbeddingFunc: func(ctx context.Context, in openaicli.EmbbedingRequest) (*openaicli.EmbeddingResponse, error) {
+			return &openaicli.EmbeddingResponse{
+				Usage: openaicli.Usage{
+					TotalTokens: 1,
+				},
+				Data: []openaicli.Embedding{
+					{
+						Embedding: []float32{1.0, 2.0, 3.0},
+					},
+				},
+			}, nil
 		},
 	}
 
-	got, err := svc.Train(context.TODO(), input)
+	repo := mockRepository{
+		StoreEmbeddingsFunc: func(ctx context.Context, in storage.StoreEmbeddingInput) error {
+			return nil
+		},
+	}
 
-	t.Log(got)
+	svc := NewService("test-api-key", &client, &repo)
 
+	input := TrainInput{
+		UserID: "test-user",
+		Model:  defaultModel,
+		Data: []io.Reader{
+			strings.NewReader("word1 word2 word3"),
+		},
+	}
+
+	collectionID, err := svc.Train(context.Background(), input)
 	require.NoError(t, err)
+
+	// Returned ID is unpredicatable, but it should not be empty
+	assert.NotEmpty(t, collectionID)
+
+	// Returned ID starts with "coll-"
+	assert.Equal(t, "coll-", collectionID[:5])
+
+	// The rest of the ID is a valid UUID
+	_, err = uuid.Parse(collectionID[5:])
+	assert.NoError(t, err)
 }
 
 func TestAsk(t *testing.T) {
-	client := openaicli.New(os.Getenv("OPENAI_API_KEY"), &http.Client{})
+	client := mockClient{
+		CreateEmbeddingFunc: func(ctx context.Context, in openaicli.EmbbedingRequest) (*openaicli.EmbeddingResponse, error) {
+			return &openaicli.EmbeddingResponse{
+				Usage: openaicli.Usage{
+					TotalTokens: 1,
+				},
+				Data: []openaicli.Embedding{
+					{
+						Embedding: []float32{1.0, 2.0, 3.0},
+					},
+				},
+			}, nil
+		},
+		CreateChatCompletitionFunc: func(ctx context.Context, in openaicli.CompletitionRequest) (*openaicli.CompletitionResponse, error) {
+			return &openaicli.CompletitionResponse{
+				Choices: []openaicli.Choice{
+					{
+						Message: openaicli.Message{
+							Content: "42",
+						},
+					},
+				},
+			}, nil
+		},
+	}
 
-	db, err := sqlx.Open(
-		"postgres",
-		"postgres://user:password@localhost:5432/testdb?sslmode=disable",
-	)
+	repo := mockRepository{
+		StoreEmbeddingsFunc: func(ctx context.Context, in storage.StoreEmbeddingInput) error {
+			return nil
+		},
+		FetchNearestNeighborFunc: func(ctx context.Context, in storage.FetchNearestNeighborInput) (text string, distance float64, err error) {
+			return "", 0.0, nil
+		},
+	}
+
+	svc := NewService("test-api-key", &client, &repo)
+
+	userID := "test-user"
+	collectionID := "coll-" + uuid.NewString()
+	question := "What is the meaning of life?"
+
+	answer, err := svc.Ask(context.Background(), userID, collectionID, question)
 	require.NoError(t, err)
 
-	repo := storage.NewPostgres(db)
-
-	svc := NewService(os.Getenv("OPENAI_API_KEY"), client, repo)
-
-	input := `What neptune the planned has to do with the roman god?`
-
-	got, err := svc.Ask(context.Background(), "user1", "coll-f59fa771-3c98-4c05-8ae4-3b069915f59e", input)
-	require.NoError(t, err)
-
-	t.Log(got)
+	assert.Equal(t, "42", answer)
 }
 
 func TestReadData(t *testing.T) {
